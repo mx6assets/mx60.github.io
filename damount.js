0dfea0d79eee67e5f4eef135eb86dee524c05b6d
MX60 <thingdontjump@gmail.com>
2024-11-16 09:59:23 -0800

    Delete damount.js

--- damount.js
+++
@@ -1,6 +0,0 @@
-  const exp = require('express');
-  const app = express();
-  let amount = 0;
-  app.use(express.static('.'));
-  app.get('/amount', (req, res) => res.send(`${amount++}`));
-  app.listen(3000);
