2b5001b1d36d5f2e397050660ae812770dccef4a
Memet Zx <zxce3@gnuweeb.org>
2025-02-21 07:06:22 +0700

    Add server configuration to Vite for development environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		// allowedHosts: ['4ee7-182-4-103-154.ngrok-free.app'] // Development
+	}
 });
