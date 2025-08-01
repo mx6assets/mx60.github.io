8d1e79f06568fefd24c0d280a98e4293d9d43184
mx6assets <thingdontjump@gmail.com>
2025-08-01 12:06:40 -0700

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-	plugins: [sveltekit()],
-	server: {
-		// allowedHosts: ['4ee7-182-4-103-154.ngrok-free.app'] // Development
-	}
-});
